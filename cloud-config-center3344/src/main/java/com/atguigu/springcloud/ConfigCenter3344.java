16354b31bd8b71da7493ae3e3089c1658b3c4358
fenghn <870137521@qq.com>
2021-03-21 13:29:57 +0800

    添加3344端口服务，用来做全局外部配置中心

---
+++ cloud-config-center3344/src/main/java/com/atguigu/springcloud/ConfigCenter3344.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author fhn
+ * @create 2021/3/20
+ * @software idea
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenter3344.class, args);
+    }
+}
