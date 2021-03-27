4971545c831e56ae1db1a8411a7db32f6844f713
fenghn <870137521@qq.com>
2021-03-27 13:12:40 +0800

    引入配置中心服务端3344以及客户端3344

--- cloud-config-client3355/src/main/java/com/atguigu/springcloud/ClientConfigMain3355.java
+++ cloud-config-client3355/src/main/java/com/atguigu/springcloud/ClientConfigMain3355.java
@@ -2,18 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author fhn
- * @create 2021/3/20
+ * @create 2021/3/27
  * @software idea
  */
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigCenter3344 {
+@EnableEurekaClient
+public class ClientConfigMain3355 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenter3344.class, args);
+        SpringApplication.run(ClientConfigMain3355.class, args);
     }
 }
---
+++ cloud-config-client3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author fhn
+ * @create 2021/3/27
+ * @software idea
+ */
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configinfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
+
+
